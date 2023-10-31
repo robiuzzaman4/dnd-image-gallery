d2dfedb622125f6b6bdc2e45de55ab69a8f48d38
ruhan <robiuzzaman4@gmail.com>
2023-10-31 22:15:02 +0600

    change images url

--- src/libs/index.js
+++ src/libs/index.js
@@ -1,13 +1,13 @@
 export const images = [
-    { id: 1, imgSrc: "/src/assets/image-11.jpeg" },
-    { id: 2, imgSrc: "/src/assets/image-2.webp" },
-    { id: 3, imgSrc: "/src/assets/image-3.webp" },
-    { id: 4, imgSrc: "/src/assets/image-4.webp" },
-    { id: 5, imgSrc: "/src/assets/image-5.webp" },
-    { id: 6, imgSrc: "/src/assets/image-6.webp" },
-    { id: 7, imgSrc: "/src/assets/image-10.jpeg" },
-    { id: 8, imgSrc: "/src/assets/image-7.webp" },
-    { id: 9, imgSrc: "/src/assets/image-8.webp" },
-    { id: 10, imgSrc: "/src/assets/image-9.webp" },
-    { id: 11, imgSrc: "/src/assets/image-1.webp" }
+    { id: 1, imgSrc: "/images/image-11.jpeg" },
+    { id: 2, imgSrc: "/images/image-2.webp" },
+    { id: 3, imgSrc: "/images/image-3.webp" },
+    { id: 4, imgSrc: "/images/image-4.webp" },
+    { id: 5, imgSrc: "/images/image-5.webp" },
+    { id: 6, imgSrc: "/images/image-6.webp" },
+    { id: 7, imgSrc: "/images/image-10.jpeg" },
+    { id: 8, imgSrc: "/images/image-7.webp" },
+    { id: 9, imgSrc: "/images/image-8.webp" },
+    { id: 10, imgSrc: "/images/image-9.webp" },
+    { id: 11, imgSrc: "/images/image-1.webp" }
 ];
